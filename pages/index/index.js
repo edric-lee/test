b6c269a349307af42ac725736e57a597332c3e38
hannibal <liyukun_money@163.com>
2019-01-08 20:36:11 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,15 @@
+// home.js
+//引入本地json数据，这里引入的就是第一步定义的json数据
+var jsonData = require('../../data/json.js');
+Page({
+  data: {
+  },
+  //我们在这里加载本地json数据
+  onLoad: function () {
+    this.setData({
+      //jsonData.dataList获取json.js里定义的json数据，并赋值给dataList
+      dataList: jsonData.dataList
+    });
+    console.log(jsonData);
+  },
+})
